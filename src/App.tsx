e802d521805c0f47a7378cbb9e09e2e9ea06f43e
junyoung-kang <junyoung.kang@navercorp.com>
2024-03-17 18:59:33 +0900

    opening

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-redeclare */
 import Location from './component/Location';
 import Intro from './component/Intro';
 import Contact from './component/Contact';
@@ -9,20 +10,82 @@ import Greeting from './component/Greeting';
 import Blank from './component/Blank';
 import Dday from './component/Dday';
 import Gallery from './component/Gallery';
+import { useEffect, useState } from 'react';
+
+import heartAnimation from './animation/heart.json';
+import Lottie from 'react-lottie';
+
+const Opening: React.FC<{
+  text: string;
+  onFinish: () => void;
+}> = ({ text, onFinish }) => {
+  const [displayText, setDisplayText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentIndex < text.length) {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        setTimeout(onFinish, 1100);
+      }
+    }, 150); // 100ms 마다 한 글자씩 표시
+
+    return () => clearTimeout(timer);
+  }, [currentIndex, text, onFinish]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: heartAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <>
+      <div className='opening'>
+        <div className='text'>
+          {displayText}
+          <Lottie options={defaultOptions} height={100} width={100}></Lottie>
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
+  const [showOpening, setShowOpening] = useState<Boolean>(true);
+
+  const handleFinishTyping = () => {
+    // 타이핑 효과가 끝나면 해당 컴포넌트를 사라지게 함
+    setShowOpening(false);
+  };
+
   return (
-    <div className='app'>
-      <Intro />
-      <Blank />
-      <Greeting />
-      <Contact />
-      <Gallery />
-      <Calendar />
-      <Dday />
-      <Location />
-      <Attendance />
-    </div>
+    <>
+      {showOpening && (
+        <Opening
+          text={'준영 산하 우리 이제 결혼합니다'}
+          onFinish={handleFinishTyping}
+        />
+      )}
+      {!showOpening && (
+        <div className='app'>
+          <Intro />
+          <Blank />
+          <Greeting />
+          <Contact />
+          <Gallery />
+          <Calendar />
+          <Dday />
+          <Location />
+          <Attendance />
+        </div>
+      )}
+    </>
   );
 }
 
