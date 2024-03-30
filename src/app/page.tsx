d41e686495de797cca16b4972cc8498d40c4da2b
junyoung-kang <junyoung.kang@navercorp.com>
2024-03-31 00:32:06 +0900

    add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,3 +7,6 @@ export function generateStaticParams() {
 export default function Page() {
   return <ClientOnly />
 }
+
+//여기서 client page를 가져오고,,., 
+//데이터 가져와서 전달하면 되는듯?  ClientOnly https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration
