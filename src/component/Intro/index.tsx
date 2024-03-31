7fbdf679c5b467cf64a5179605a0a8a632183c97
junyoung-kang <junyoung.kang@navercorp.com>
2024-03-31 15:11:13 +0900

    대문 날짜 장소 추가

--- src/component/Intro/index.tsx
+++ src/component/Intro/index.tsx
@@ -12,11 +12,25 @@ const Intro = () => {
           <div className='name'>
             <div className='txt'>강</div>
             <div className='txt'>준</div>
-            <div className='txt'>영</div>
+            <div className='young'>영</div>
             <div className='ring'>💍</div>
             <div className='txt'>최</div>
             <div className='txt'>산</div>
             <div className='txt'>하</div>
+          </div>
+        </div>
+      </div>
+      <div className='celebrate'>
+        <div className='txt'>We're getting married</div>
+        <div className='info'>
+          <div className='date'>
+            <p>2024년 8월 25일</p>
+            <p>일요일 PM 12시 30분</p>
+          </div>
+          {/* <div className='separator'></div> */}
+          <div className='address'>
+            <p>양재 aT 포레</p>
+            <p>5층 단독홀</p>
           </div>
         </div>
       </div>
