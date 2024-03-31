1d212d6f748ad96657edaf997cdc368b5c53d121
junyoung-kang <junyoung.kang@navercorp.com>
2024-03-31 15:46:21 +0900

    1안

--- src/component/Intro/index.tsx
+++ src/component/Intro/index.tsx
@@ -19,18 +19,18 @@ const Intro = () => {
             <div className='txt'>하</div>
           </div>
         </div>
-      </div>
-      <div className='celebrate'>
-        <div className='txt'>We're getting married</div>
-        <div className='info'>
-          <div className='date'>
-            <p>2024년 8월 25일</p>
-            <p>일요일 PM 12시 30분</p>
-          </div>
-          {/* <div className='separator'></div> */}
-          <div className='address'>
-            <p>양재 aT 포레</p>
-            <p>5층 단독홀</p>
+        <div className='celebrate'>
+          <div className='txt'>We're getting married</div>
+          <div className='info'>
+            <div className='date'>
+              <p>2024년 8월 25일</p>
+              <p>일요일 PM 12시 30분</p>
+            </div>
+            {/* <div className='separator'></div> */}
+            <div className='address'>
+              <p>양재 aT 포레</p>
+              <p>5층 단독홀</p>
+            </div>
           </div>
         </div>
       </div>
