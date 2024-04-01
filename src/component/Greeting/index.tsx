60f4da83ba068e473c7eaf9306ef16758fedc06c
junyoung-kang <junyoung.kang@navercorp.com>
2024-04-01 22:17:01 +0900

    greeting 글씨 체 및 문구 변경 완료

--- src/component/Greeting/index.tsx
+++ src/component/Greeting/index.tsx
@@ -5,12 +5,23 @@ const Greeting = () => {
     <div className='sectionMg'>
       <div className='sectionHead'>
         <div className='enstr'>INVITATION</div>
-        <span className='ment'>
+        {/* <span className='name'>
           <strong>소중한 분들을 초대합니다</strong>
-        </span>
+        </span> */}
       </div>
       <div className='introMent'>
-        <p>
+        사랑하는 이들에게 나란한 이름으로 <br />
+        보내는  첫 번째 편지 <br />
+        늘 곁에서 아껴주신 고마운 분들을 모십니다 
+        <br />
+        <br />
+        
+        싱그러운 여름, <br />
+        세상에 둘도 없는 제 소중한 친구와 <br />
+        앞으로도 함께 행복하고 싶어서 <br />  
+        부부라는 이름으로 나아가려 합니다.
+
+        {/* <p>
           저희 두사람의 작은 만남이
           <br />
           사랑의 결실을 이루어
@@ -33,7 +44,7 @@ const Greeting = () => {
           지켜나갈 수 있게
           <br />
           앞날을 축복해 주시면 감사하겠습니다.
-        </p>
+        </p> */}
       </div>
     </div>
   );
