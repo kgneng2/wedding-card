d8a841c1660fa9459c9ce29452f9df5570565409
junyoung-kang <junyoung.kang@navercorp.com>
2024-04-03 02:16:43 +0900

    방명록 구현중

--- src/component/GuestBook/index.tsx
+++ src/component/GuestBook/index.tsx
@@ -1,18 +1,36 @@
 import { useEffect, useState } from 'react';
-import { Button, ConfigProvider, Layout, List } from 'antd';
+import { Avatar, Button, ConfigProvider, Layout, List, Skeleton } from 'antd';
 import './styles.scss';
 import GuestBookForm from 'src/component/GuestBook/guestBookForm';
+import { CloseOutlined } from '@ant-design/icons';
 // import guestbookAPI from '../lib/guestbookAPI';
 
 const { Header, Content } = Layout;
 
+interface IData {
+  name: string;
+  password: string;
+  content: string;
+  date: string;
+}
+
 const GuestbookPage = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<IData[]>([
+    {
+      name: '강준영',
+      password: '1234',
+      content: 'asdflhdasnlkndasklfnasdlfndaslkfndsalnf las nfsdakfndsalknlk',
+      date: '2024-02-10',
+    },
+  ]);
   const [openForm, setOpenForm] = useState(false);
+
+  const [loading, setLoading] = useState(false);
 
   const toggleOpenForm = () => {
     setOpenForm(!openForm);
   };
+
   //   useEffect(() => {
   //     const fetchGuestbook = async () => {
   //       const data = await guestbookAPI.fetchGuestbook();
@@ -28,57 +46,86 @@ const GuestbookPage = () => {
   //   };
 
   return (
-    <div className='guest-book'>
-      <div className='header'>
-        <div className='title'>Guest Book</div>
-        <div className='body'>
-          신랑 & 신부의 행복한 앞날을 위해 <br />
-          따듯한 덕담 한 말씀 남겨주세요. <br />
-          소중한 추억으로 간직하겠습니다.
-        </div>
-      </div>
-      <ConfigProvider
-        theme={{
-          components: {
-            Button: {
-              defaultActiveBorderColor: '#a2d3cd',
-              defaultActiveColor: '#a2d3cd',
-              defaultHoverBorderColor: '#a2d3cd',
-              defaultHoverColor: '#a2d3cd',
-              defaultBg: '#a2d3cd',
-              defaultBorderColor: '#a2d3cd',
-              defaultColor: '#ffff',
-              paddingInline: 20,
-            },
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            defaultActiveBorderColor: '#a2d3cd',
+            defaultActiveColor: '#a2d3cd',
+            defaultHoverBorderColor: '#a2d3cd',
+            defaultHoverColor: '#a2d3cd',
+            defaultBg: '#a2d3cd',
+            defaultBorderColor: '#a2d3cd',
+            defaultColor: '#ffff',
+            paddingInline: 20,
           },
-        }}
-      >
-        <Button onClick={toggleOpenForm}>축하 메시지 작성하기</Button>
-        {openForm && (
-          <>
-            <div className='overlay' onClick={toggleOpenForm}></div>
-            <GuestBookForm
-              onSubmit={console.log}
-              toggleOpenForm={toggleOpenForm}
-            />
-          </>
-        )}
-      </ConfigProvider>
-      <Layout>
-        <Content style={{ padding: '20px' }}>
-          <List
-            className='demo-loadmore-list'
-            itemLayout='horizontal'
-            dataSource={list}
-            renderItem={(item) => (
-              <List.Item>
-                <List.Item.Meta title={'강준영'} />
-              </List.Item>
-            )}
-          />
-        </Content>
-      </Layout>
-    </div>
+        },
+      }}
+    >
+      <div className='guest-book'>
+        <div className='header'>
+          <div className='title'>Guest Book</div>
+          <div className='body'>
+            신랑 & 신부의 행복한 앞날을 위해 <br />
+            따듯한 덕담 한 말씀 남겨주세요. <br />
+            소중한 추억으로 간직하겠습니다.
+          </div>
+
+          <Button onClick={toggleOpenForm}>축하 메시지 작성하기</Button>
+          {openForm && (
+            <>
+              <div className='overlay' onClick={toggleOpenForm}></div>
+              <GuestBookForm
+                onSubmit={console.log}
+                toggleOpenForm={toggleOpenForm}
+              />
+            </>
+          )}
+        </div>
+        <List
+          className='demo-loadmore-list container'
+          itemLayout='horizontal'
+          dataSource={list}
+          renderItem={(item, index) => (
+            <List.Item
+              actions={[
+                <CloseOutlined
+                  onClick={() => {
+                    console.log('삭제');
+                  }}
+                />,
+              ]}
+            >
+              <Skeleton avatar title={false} loading={loading} active>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar
+                      src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                    />
+                  }
+                  title={
+                    <div>
+                      From {item.name}{' '}
+                      <span
+                        style={{
+                          paddingLeft: 10,
+                          fontWeight: 200,
+                          fontSize: 12,
+                        }}
+                      >
+                        {item.date}
+                      </span>
+                    </div>
+                  }
+                  description={item.content}
+                />
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+        <Button style={{ width: 150 }}> 더보기 </Button>
+      </div>
+    </ConfigProvider>
   );
 };
 
