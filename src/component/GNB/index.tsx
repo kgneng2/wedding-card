98f1bb62cc343c444ac501b837445babcb5504cb
junyoung-kang <junyoung.kang@navercorp.com>
2024-04-07 01:19:05 +0900

    tmp

--- src/component/GNB/index.tsx
+++ src/component/GNB/index.tsx
@@ -3,18 +3,18 @@ import './styles.scss';
 
 const GNB = () => {
   return (
-    <nav className='navbar'>
-      <ul className='navList'>
-        <li className='navItem'>
-          <Link href='/'>
-            갤러리 보기
-          </Link>
-        </li>
-        <li className={'navItem'}>
-          <Link href='/invitation'>결혼식 안내</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className='gnb'>
+      <nav className='navbar'>
+        <ul className='navList'>
+          <li className='navItem'>
+            <Link href='/'>갤러리 보기</Link>
+          </li>
+          <li className={'navItem'}>
+            <Link href='/invitation'>결혼식 안내</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
