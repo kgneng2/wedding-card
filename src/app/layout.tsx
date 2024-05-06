379a32f04012644b939433a3064252dcb41e832b
junyoung-kang <junyoung.kang@navercorp.com>
2024-05-06 15:37:00 +0900

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
 import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: '강준영 ❤️ 최산하 우리 결혼합니다 ',
   description: '주녕이와 사나의 모바일 청첩장',
 };
 
-
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <html lang='en'>
       <head>
-        <link href="https://hangeul.pstatic.net/hangeul_static/css/maru-buri.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        <link
+          href='https://hangeul.pstatic.net/hangeul_static/css/maru-buri.css'
+          rel='stylesheet'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        ></meta>
         <script
           type='text/javascript'
           src='https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=usk1bgr0t6'
@@ -25,6 +29,7 @@ export default function RootLayout({
       </head>
       <body>
         <div id='root'>{children}</div>
+        <Analytics />
       </body>
     </html>
   );
