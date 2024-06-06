c5dce4a2bcdcfb56e3faacaac0c711aefd958fbf
junyoung-kang <junyoung.kang@navercorp.com>
2024-06-07 01:17:00 +0900

    cache 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,6 @@
 const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
   // distDir: './dist', // Changes the build output directory to `./dist/`.
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/images/gallery',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=604800, immutable', // 7일 동안 캐시
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
