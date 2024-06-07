4dd4db4b966b7f5ad8502359c77b4b8511de88fb
junyoung-kang <junyoung.kang@navercorp.com>
2024-06-07 14:21:41 +0900

    main 이미지 src로 수정

--- src/component/Intro/index.tsx
+++ src/component/Intro/index.tsx
@@ -1,15 +1,15 @@
 import './styles.scss';
-import main1 from '../../../public/images/main/main1.jpg';
-import main2 from '../../../public/images/main/main2.jpg';
-import main3 from '../../../public/images/main/main3.jpg';
-import main4 from '../../../public/images/main/main4.jpg';
-import main6 from '../../../public/images/main/main6.jpg';
 import { Carousel } from 'antd';
 import Image from 'next/image';
 
 const Intro = () => {
-  // const doorImageList = [main1.src, main2.src, main3.src, main4.src, main6.src, main5.src];
-  const doorImageList = [main1, main2, main3, main4, main6];
+  const doorImageList = [
+    '/images/main/main1.jpg',
+    '/images/main/main2.jpg',
+    '/images/main/main3.jpg',
+    '/images/main/main4.jpg',
+    '/images/main/main6.jpg',
+  ];
 
   return (
     <>
@@ -34,8 +34,8 @@ const Intro = () => {
             dots={false}
           >
             {doorImageList.map((img) => {
-              return <Image src={img} alt={'images'} layout='responsive'/>
-              // return <img src={img.src} className='image'></img>;
+              // return <Image src={img} alt={'images'}  fill/>
+              return <img src={img} className='image'></img>;
             })}
           </Carousel>
         </div>
